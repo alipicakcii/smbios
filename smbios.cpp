2fcf0fea188008902b4565a651afadbe96daba4f
alipicakcii <apicakci140@gmail.com>
2024-06-25 16:13:19 +0300

    Initial commit

---
+++ smbios.cpp
@@ -0,0 +1,123 @@
+#include <firmware/smbios.hh>
+
+
+namespace firmware
+{
+	
+
+	size_t SMBIOS::smbios_struct_len(SMBIOSHeader *hd)
+	{
+		size_t i;
+		const char *strtab = (char *)hd + hd->len;
+		for (i = 1; strtab[i - 1] != '\0' || strtab[i] != '\0'; i++)
+			;
+		return hd->len + i + 1;
+	}
+
+	SMBIOS::SMBIOS()
+	{
+
+		this->_eps = this->find_eps();
+		this->_str_start = nullptr;
+		this->_str_end   = nullptr;
+		this->_type_next_number =  0;
+		
+	}
+
+	void SMBIOS::print_string()
+	{
+		if (this->_str_start == nullptr ||this->_str_end == nullptr  )return;
+
+
+		while((uint32_t) this->_str_start < (uint32_t) this->_str_end)
+		{
+			
+			if (this->_str_start[0] == '\0' && this->_str_start[1] == '\0') break;
+
+			while(*this->_str_start == '\0')
+			{
+				kprintf(" ");
+				this->_str_start++;
+
+			}
+	
+			char c = *this->_str_start++;
+			_kprintf("%c", c);
+		}
+	}
+
+	SmBiosStructure * SMBIOS::next_structure()
+	{
+		if (this->_eps == nullptr) return nullptr;
+
+		SmBiosStructure  * type = reinterpret_cast<SmBiosStructure *>(this->_eps->structure_table_address);
+
+
+		char *buf;
+		char  buf_len;
+		for (int i  = 0; i < this->_type_next_number; i++)
+		{
+			buf_len = this->smbios_struct_len(& type->header);
+			buf     = ((char*) type + buf_len);
+			type    =  reinterpret_cast<SmBiosStructure*>(buf);
+		}
+
+
+		char * str_start = (char*)type + type->header.len;
+		char * str_end   = (char*) str_start + 64; 
+		this->_str_start = str_start;
+		this->_str_end   = str_end;
+		this->_type_next_number++;
+		if (
+				type->header.type >= 127 
+				|| 
+				type->header.len <=0
+		   )
+		{
+			return nullptr;
+		}
+		return type;
+	}
+
+	char * SMBIOS::string_start()
+	{
+		return this->_str_start;
+
+	}
+	char * SMBIOS::string_end()
+	{
+		return this->_str_end;
+	}
+	
+	Eps * SMBIOS::find_eps()
+	{
+		char *_start = (char*)START_ADDR;
+		char *_end = (char*)END_ADDR;
+		bool checksum = false;
+		while((uint32_t)_start < (uint32_t)_end)
+		{
+			if (
+					_start[0] == '_' &&
+					_start[1] == 'S' &&
+					_start[2] == 'M' &&
+					_start[3] == '_' 
+
+			   )
+			{
+				checksum = true;
+				break;
+			}
+			_start++;
+		}
+		if (!checksum)return nullptr;
+		return reinterpret_cast<Eps*>(_start);
+	}
+	Eps * SMBIOS::get_eps()
+	{
+		
+		return this->_eps;
+	}
+
+	
+	
+}
---
+++ smbios.hh
@@ -0,0 +1,192 @@
+#ifndef FIRMWRAE_SM_BIOS
+#define FIRMWRAE_SM_BIOS
+
+#include <string.h>
+#include <stdint.h>
+#include <library/kprint.hh>
+#include <library/type_traits.hh>
+
+
+#define START_ADDR 				0x000F0000
+#define END_ADDR 				0x000FFFFF
+
+
+namespace firmware
+{
+
+	// bütün tablolar varsa sıralama
+	// bu şekilde gitmekedir ardışık ola
+	enum SmBiosType  : char
+	{
+		BIOS_Information      =0,    
+		System_Information    =1,  
+		System_Enclosure      =3,
+		Processor_Information =4, 
+		Cache_Information     =7,
+		System_Slots	      =9,
+		Physical_Memory_Arr   =16,
+		Memory_Device         =17 ,
+		Memory_Array          =19,
+		Memory_Dev_Map_Addr   =20,
+		System_Boot_Infor     =32,
+
+
+		inactive	      =126,
+		End_of_Table          =127
+	};
+
+	struct Eps
+	{
+		uint8_t anchor_string[4];
+		uint8_t checksum;
+		uint8_t length;
+		uint8_t major;
+		uint8_t minor;
+		uint16_t max_size;
+		uint8_t revision;
+		uint8_t format_area[5];
+		uint8_t intermediate_anchor_string[5];
+		uint8_t intermediate_checksum;
+		uint16_t structure_table_length;
+		uint32_t structure_table_address;
+		uint16_t number;
+		uint8_t bcd_revision;
+
+
+	}__attribute__((packed));
+
+
+	
+	struct SMBIOSHeader {
+		
+		uint8_t type;
+		uint8_t len;
+		uint16_t handle;
+
+	}__attribute__((packed));
+
+
+	struct SmBiosStructure
+	{
+		SMBIOSHeader header;
+
+	}__attribute__((packed));
+
+	struct BiosInformation 
+		: public SmBiosStructure
+	{
+		uint8_t  vendor;
+		uint8_t  version;
+		uint16_t starting_addr;
+		uint8_t  release_date;
+		uint8_t  rom_size;
+		uint64_t characteristics;
+		uint8_t  characteristics_ex;
+
+	}__attribute__((packed));
+
+
+	struct SystemInformation
+		:public SmBiosStructure
+	{
+		uint8_t manufacturer;
+		uint8_t product_name;
+		uint8_t version;
+		uint8_t serial_number;
+		uint8_t uuid[16];
+		uint8_t wake_up_type;
+		uint8_t sku_number;
+		uint8_t family;
+
+
+
+	}__attribute__((packed));
+
+	struct SystemEnclosure
+		: public SmBiosStructure
+	{
+		uint8_t manufacturer;
+		uint8_t type;
+		uint8_t version;
+		uint8_t serial_number;
+		uint8_t asset_tag_number;
+		uint8_t boot_up_state;
+		uint8_t power_supply_state;
+		uint8_t thermal_state;
+		uint8_t security_status;
+		uint16_t oem_defined;
+		uint8_t height;
+		uint8_t number_of_power_cords;
+		uint8_t contained_element_count;
+		uint8_t contained_element_record_len;
+
+
+	}__attribute__((packed));
+
+
+	struct ProcInformation
+		: public SmBiosStructure
+	{
+		uint8_t socket_designation;
+		uint8_t type;
+		uint8_t family;
+		uint8_t manufacturer;
+		uint64_t id;
+		uint8_t version;
+		uint8_t voltage;
+		uint16_t external_clock;
+		uint16_t max_speed;
+		uint16_t current_speed;
+		uint8_t status;
+		uint8_t upgrade;
+		uint16_t l1_cahce_handle;
+		uint16_t l2_cahce_handle;
+		uint16_t l3_cahce_handle;
+		uint8_t serial_number;
+		uint8_t asset_tag;
+		uint8_t part_number;
+
+	}__attribute__((packed));
+
+	struct CacheInformation
+		:public  SmBiosStructure
+	{
+		
+		uint8_t socket_dsesignation;
+		uint16_t cache_configuration;
+		uint16_t maximum_cache_size;
+		uint16_t installed_size;
+		uint16_t supported_sram_type;
+		uint16_t current_sram_type;
+		uint8_t  cahce_speed;
+		uint8_t error_correction_type;
+		uint8_t system_cache_type;
+		uint8_t associativity;
+
+
+
+	}__attribute__((packed));
+
+	class SMBIOS
+	{
+		private:
+			Eps*  _eps;
+			Eps*  find_eps();
+			char* _str_start;
+			char* _str_end;
+			int   _type_next_number;
+		public:
+			SMBIOS(); 
+			Eps *get_eps();
+			size_t smbios_struct_len(SMBIOSHeader *hd);
+			void print_string();
+			SmBiosStructure * next_structure();
+			char * string_start();
+			char * string_end();
+			
+		
+	};
+}
+
+
+#endif
